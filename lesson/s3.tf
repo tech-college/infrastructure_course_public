fd423d4adb2e01ae3b9fc5b565f64bddd2189f33
iwasawa <hiroaki.iwasawa@outlook.com>
2023-06-04 23:31:16 +0900

    Tech-College中級　AWSコース　#12(s3,vpc追加)

---
+++ lesson/s3.tf
@@ -0,0 +1,90 @@
+# Private S3 bucket
+resource "aws_s3_bucket" "private" {
+  bucket = "s3-YOUR-BUCKET-NAME-private"
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "private" {
+  bucket = aws_s3_bucket.private.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "private" {
+  bucket = aws_s3_bucket.private.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "private" {
+  bucket = aws_s3_bucket.private.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Public S3 bucket
+resource "aws_s3_bucket" "public" {
+  bucket = "s3-YOUR-BUCKET-NAME-public"
+}
+
+resource "aws_s3_bucket_acl" "public" {
+  bucket = aws_s3_bucket.public.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "public" {
+  bucket = aws_s3_bucket.public.id
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+# Log S3 bucket
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "s3-YOUR-BUCKET-NAME-alb-log"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+
+  rule {
+    id     = "alb_log"
+    status = "Enabled"
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["582318560864"] # ap-northeast-1 region
+    }
+  }
+}
---
+++ lesson/vpc.tf
@@ -0,0 +1,186 @@
+# VPC
+resource "aws_vpc" "example" {
+  cidr_block           = "10.X.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "YOUR-VPC-NAME"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+# Route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+}
+
+# # Public subnet
+# resource "aws_subnet" "public" {
+#   vpc_id                  = aws_vpc.example.id
+#   cidr_block              = "10.X.0.0/24"
+#   map_public_ip_on_launch = true
+#   availability_zone       = "ap-northeast-1a"
+# }
+
+# Route(public)
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# # Route association
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public.id
+# }
+
+# # Private subnet
+# resource "aws_subnet" "private" {
+#   vpc_id                  = aws_vpc.example.id
+#   cidr_block              = "10.X.64.0/24"
+#   availability_zone       = "ap-northeast-1a"
+#   map_public_ip_on_launch = false
+# }
+
+# # Route(private)
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.example.id
+# }
+
+# # Route association
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private.id
+#   route_table_id = aws_route_table.private.id
+# }
+
+# # EIP for NAT Gateway
+# resource "aws_eip" "nat_gateway" {
+#   vpc = true
+#   depends_on = [
+#     aws_internet_gateway.example
+#   ]
+# }
+
+# # NAT Gateway
+# resource "aws_nat_gateway" "example" {
+#   allocation_id = aws_eip.nat_gateway.id
+#   subnet_id     = aws_subnet.public.id
+#   depends_on = [
+#     aws_internet_gateway.example
+#   ]
+# }
+
+# # Route(private -> NAT Gateway)
+# resource "aws_route" "private" {
+#   route_table_id         = aws_route_table.private
+#   nat_gateway_id         = aws_nat_gateway.example.id
+#   destination_cidr_block = "0.0.0.0/0"
+# }
+
+# Multi AZ Public subnet
+resource "aws_subnet" "public_0" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.X.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.X.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table_association" "public_0" {
+  subnet_id      = aws_subnet.public_0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Multi AZ Private subnet
+resource "aws_subnet" "private_0" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.X.65.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.X.66.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+
+# Multi AZ NAT Gateway
+resource "aws_eip" "nat_gateway_0" {
+  vpc = true
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc = true
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id     = aws_subnet.public_0.id
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+# Multi AZ Route table
+resource "aws_route_table" "private_0" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route" "private_0" {
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "private_0" {
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
