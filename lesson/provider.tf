4ecc7776c1ea433855aa8bd87c026071a5a8c7e4
iwasawa <hiroaki.iwasawa@outlook.com>
2023-08-29 01:51:21 +0900

    #21　provider.tf追加

---
+++ lesson/provider.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "tech-college"
+}
+
+provider "github" {
+  token = "YOUR-GITHUB-TOKEN"
+  owner = "YOUR-OWNER-NAME"
+}
+
+terraform {
+  required_version = "1.3.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67.0"
+    }
+  }
+}
